3cb119dd091142fd2a862950b4c0bcf9262c4b8c
Luis Mario Cervantes Suarez <zs21015977@estudiantes.uv.mx>
2024-11-14 15:14:38 -0600

    Agregar archivos iniciales para la aplicación empleados y configuración del proyecto Django

---
+++ empleados/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+# Create your models here.
+
+# Opciones de genero
+GENERO = (
+    ('M', 'Masculino'),
+    ('F', 'Femenino'),
+    ('O', 'Otro')
+)
+
+# Opciones de estado civil
+ESTADO_CIVIL = (
+    ('S', 'Soltero'),
+    ('C', 'Casado'),
+    ('D', 'Divorciado'),
+    ('V', 'Viudo')
+)
+
+# Opciones de escolaridad
+ESCOLARIDAD = (
+    ('P', 'Primaria'),
+    ('S', 'Secundaria'),
+    ('B', 'Bachillerato'),
+    ('L', 'Licenciatura'),
+    ('M', 'Maestria'),
+    ('D', 'Doctorado')
+)
+
+class Empleado(models.Model):
+    # Nombre
+    primer_nombre = models.CharField(max_length=50, verbose_name="Primer nombre", null=False, blank=False)
+    segundo_nombre = models.CharField(max_length=50, verbose_name="Segundo nombre", null=True, blank=True)
+    primer_apellido = models.CharField(max_length=50, verbose_name="Primer apellido", null=False, blank=False)
+    segundo_apellido = models.CharField(max_length=50, verbose_name="Segundo apellido", null=False, blank=False)
+    # Contacto
+    telefono = models.CharField(max_length=10, verbose_name="Telefono", null=True, blank=True)
+    correo = models.EmailField(max_length=100, verbose_name="Correo", null=True, blank=True)
+    # Ubicacion
+    calle = models.CharField(max_length=100, verbose_name="Calle", null=True, blank=True)
+    numero_exterior = models.CharField(max_length=10, verbose_name="Numero exterior", null=True, blank=True)
+    numero_interior = models.CharField(max_length=10, verbose_name="Numero interior", null=True, blank=True)
+    colonia = models.CharField(max_length=100, verbose_name="Colonia", null=True, blank=True)
+    codigo_postal = models.CharField(max_length=5, verbose_name="Codigo postal", null=True, blank=True)
+    ciudad = models.CharField(max_length=100, verbose_name="Ciudad", null=True, blank=True)
+    estado = models.CharField(max_length=100, verbose_name="Estado", null=True, blank=True)
+    pais = models.CharField(max_length=100, verbose_name="Pais", null=True, blank=True)
+    # Informacion personal
+    fecha_nacimiento = models.DateField(verbose_name="Fecha de nacimiento", null=True, blank=True)
+    genero = models.CharField(max_length=1, choices=GENERO, verbose_name="Genero", null=True, blank=True)
+    estado_civil = models.CharField(max_length=1, choices=ESTADO_CIVIL, verbose_name="Estado civil", null=True, blank=True)
+    escolaridad = models.CharField(max_length=1, choices=ESCOLARIDAD, verbose_name="Escolaridad", null=True, blank=True)
---
+++ router/router.py
@@ -0,0 +1,54 @@
+class AdministradorDBRouter:
+    # Aplicaciones
+    router_app_labels = {
+        'auth', 'contenttypes', 'sessions', 'admin'
+    }
+    
+    # Metodos
+    
+    # Leer datos
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label in self.router_app_labels:
+            # El dato se toma de apps.py y de la clase meta de los models.py
+            return 'default'
+        return None
+    
+    # Escribir datos
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label in self.router_app_labels:
+            return 'default'
+        return None
+    
+    # Permitir la relacion
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._meta.app_label in self.router_app_labels or obj2.meta.app_label in self.router_app_labels:
+            return True
+        return None
+    
+    # Permitir la migracion
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label in self.router_app_labels:
+            return db == 'default'
+        return None
+
+class EmpleadosRouter:
+    # Metodos
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'empleados':
+            return 'empleados'
+        return None
+    
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'empleados':
+            return 'empleados'
+        return None
+    
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._meta.app_label == 'empleados' or obj2.meta.app_label == 'empleados':
+            return True
+        return None
+    
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label == 'empleados':
+            return db == 'empleados'
+        return None
